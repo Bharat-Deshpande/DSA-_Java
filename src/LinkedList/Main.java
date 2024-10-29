27e5a98d2db476d477bafdfbc63f3f68423c4c77
Bharat-Deshpande <bharatdeshpande007@gmail.com>
2024-10-29 15:19:41 +0530

    Added new files

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,22 @@
+package LinkedList;
+ public class Main{
+     public static void main(String[] args) {
+         linkedlist list = new linkedlist();
+         list.insertFirst(3);
+         list.insertFirst(2);
+         list.insertFirst(8);
+         list.insertFirst(17);
+         list.insertLast(99);
+         list.display();
+//         System.out.println(list.deleteFirst());
+//         list.display();
+//         System.out.println(list.deleteLast());
+//         list.display();
+//         list.delete(1);
+//         list.display();
+
+//         System.out.println(list.find(8));
+
+
+     }
+ }
--- src/linkedList.java
+++
@@ -1,5 +0,0 @@
-public class linkedList {
-    public static void main(String[] args) {
-        System.out.println("bharat");
-    }
-}
